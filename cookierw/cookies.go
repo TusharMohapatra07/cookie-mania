7354567bcbdc36acf0e51067255b9d6b8e5e327b
TusharMohapatra07 <tusharmohapatra.gig@gmail.com>
2024-12-31 23:32:37 +0530

    add cookierw package

--- cookierw/cookies.go
+++ cookierw/cookies.go
@@ -1,3 +1,42 @@
 package cookierw
 
-// cookie read write
+import (
+	"encoding/base64"
+	"errors"
+	"net/http"
+)
+
+// COOKIE READ WRITE: A good way to ensure that the cookie value only contains
+//the US ASCII character is to base-64 encode the cookie value before writing
+//the value
+
+var (
+	ErrValueTooLong = errors.New("cookie value too long")
+	ErrInvalidValue = errors.New("invalid cookie value")
+)
+
+func Write(w http.ResponseWriter, cookie http.Cookie) error {
+	cookie.Value = base64.URLEncoding.EncodeToString([]byte(cookie.Value))
+
+	if len(cookie.String()) > 4096 {
+		return ErrValueTooLong
+	}
+
+	http.SetCookie(w, &cookie)
+
+	return nil
+}
+
+func Read(r *http.Request, name string) (string, error) {
+	cookie, err := r.Cookie(name)
+	if err != nil {
+		return "", nil
+	}
+
+	value, err := base64.URLEncoding.DecodeString(cookie.Value)
+	if err != nil {
+		return "", ErrInvalidValue
+	}
+
+	return string(value), nil
+}
